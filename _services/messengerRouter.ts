840f50dc617b8e5e85cdacf200f410cd25f64b21
Maciej Wojdyła <maciejwojdyla@10g.pl>
2022-09-20 20:11:28 +0200

    wa

--- _services/messengerRouter.ts
+++ _services/messengerRouter.ts
@@ -2,7 +2,7 @@ const
 	express = require('express'),
 	bodyParser = require('body-parser'),
 	axios = require("axios"),
-    env = require("dotenv")
+	env = require("dotenv")
 
 export const messengerRouter = express.Router()
 messengerRouter.use(bodyParser.urlencoded({ extended: true }))
@@ -10,71 +10,71 @@ messengerRouter.use(bodyParser.json())
 messengerRouter.use(express.static('public'))
 messengerRouter.use(express.json())
 
-messengerRouter.post('/webhook', (req, res) => {  
+messengerRouter.post('/webhook', (req, res) => {
 	let body = req.body;
 	console.log(body);
 	console.log(body.entry[0].messaging[0].message.text);
 	if (body.object === 'page') {
-		body.entry.forEach( (entry:any) => {
-            //sample: dimwishlist:item=821154603&perks=3250034553,2420895100,3523296417			
+		body.entry.forEach((entry: any) => {
+			//sample: dimwishlist:item=821154603&perks=3250034553,2420895100,3523296417			
 			let webhookEvent = entry.messaging[0];
 			let senderPsid = webhookEvent.sender.id;
 			let senderMessage = webhookEvent.message.text
-			if(senderMessage === `help`) {
+			if (senderMessage === `help`) {
 				sendMessage(senderPsid, sendHelpResponse())
 			}
-			
-		
-		
+
+
+
 		})
 		res.status(200).send('EVENT_RECEIVED');
 	} else {
 		res.sendStatus(404);
 	}
-	
+
 	res.sendStatus(200)
 })
 
 messengerRouter.get('/webhook', (req, res) => {
-		console.log("INCOMING GET REQUEST")
-		let VERIFY_TOKEN = "43256233"
-		let mode = req.query['hub.mode'];
-		let token = req.query['hub.verify_token'];
-		let challenge = req.query['hub.challenge'];
-		if (mode && token) {
-			if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-				console.log('WEBHOOK_VERIFIED');
-				res.status(200).send(challenge);			
-			} else {
-				res.sendStatus(403);      
-			}
+	console.log("INCOMING GET REQUEST")
+	let VERIFY_TOKEN = "43256233"
+	let mode = req.query['hub.mode'];
+	let token = req.query['hub.verify_token'];
+	let challenge = req.query['hub.challenge'];
+	if (mode && token) {
+		if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+			console.log('WEBHOOK_VERIFIED');
+			res.status(200).send(challenge);
+		} else {
+			res.sendStatus(403);
 		}
+	}
 });
 
-function sendMessage(sender_psid:string, message:string) {
+function sendMessage(sender_psid: string, message: string) {
 	let data = {
 		headers: {
 			'content-Type': 'messengerRouterlication/json'
 		},
-		'recipient':{
-			'id':sender_psid
+		'recipient': {
+			'id': sender_psid
 		},
 		'message': {
 			'text': message
-		   }   
-	}   
-	axios.post(`https://graph.facebook.com/v12.0/me/messages?access_token=${env.mess_token}`, data)
-	   .then(res => { })
-	   .catch(err =>{console.log(err.response)}) 
+		}
+	}
+	axios.post(`https://graph.facebook.com/v12.0/me/messages?access_token=${process.env.mess_token}`, data)
+		.then(res => { })
+		.catch(err => { console.log(err.response) })
 }
 
-function validate(text:string){
-    let regex1:RegExp = /dimwishlist:item=\d+&perks=(\d+(,\d+)+)/
-    let regex2:RegExp = /dimwishlist:item=\d/
-    if(regex1.test(text) && regex2.test(text)){
-        return true
-    }
-    return false
+function validate(text: string) {
+	let regex1: RegExp = /dimwishlist:item=\d+&perks=(\d+(,\d+)+)/
+	let regex2: RegExp = /dimwishlist:item=\d/
+	if (regex1.test(text) && regex2.test(text)) {
+		return true
+	}
+	return false
 }
 
 function sendHelpResponse() {
