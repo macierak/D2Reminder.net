95c98fd8c18b27fd37b75f15f3198989917f9cab
Maciej Wojdyła <maciejwojdyla@10g.pl>
2022-09-21 21:29:10 +0200

    d

--- _modules/FacebookAPI.ts
+++ _modules/FacebookAPI.ts
@@ -5,7 +5,9 @@ export async function sendMess(fb_id:string, textMess:string){
     let data = {
         headers: {'content-Type': 'application/json'},
         'recipient':{'id':fb_id},
-        'message': {'text': textMess }   
+        'message': {'text': textMess },
+        'messaging_type': 'MESSAGE_TAG',
+        'tag': "CONFIRMED_EVENT_UPDATE"  
     }   
     axios.post(`https://graph.facebook.com/v12.0/me/messages?access_token=${process.env.mess_token}`, data).then(res => { }).catch(err =>{console.log(err.response); console.log("Messaging Error")}) 
 }
